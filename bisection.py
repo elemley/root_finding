9d46475dcbdd17739a8f4b5f6b4b19965ff9e736
elemley <elemley@gmail.com>
2019-02-20 13:51:36 -0600

    Updates for Spring 2019

--- bisection.py
+++ bisection.py
@@ -3,44 +3,49 @@ from psm_plot import *
 
 #Note this is specified def f_string():
 ##You can choose not to use this... Just delete it in that case
-def f_string():
-    fn_string = "x-2*exp(-x)"
-    return fn_string
 
 def f(x):
     #tmp = 1.2*pow(x,3)+2*pow(x,2)-20*x-10
-    #tmp = x - 2.0*exp(-x)
-    #If you are not using f_string uncomment the line above
-    #If you are not using the f_string comment out the next two lines
-    string = f_string()
-    tmp = eval(string)
+    tmp = x - 2.0*exp(-x)
     return tmp
 
 def main():
     a = 0.0         #define and a and b (bracketing values)
     b = 1.0
-    dx = .01
-    N = (int)((b-a)/dx)
-    x = []
-    fx = []
-    for i in range(0,N+1):
-        x.append(i*dx+a)
-        fx.append(f(x[i]))
-    print(x)
-    #print(fx)
+    err_stop = 1e-5
+    if f(a)*f(b)< 0:
+        rel_err = 1.1*err_stop
+        max_iter = 200
+        xns_old = (a+b)/2
+        for i in range(0,max_iter):
+            xns = (a+b)/2
+            #put code here to decide if f(a)*f(xns) is < 0
+                #if < 0 what do you do (look at notes)
 
-    #if you are not using f_string type in the name of the function on the next line
-    function_name = f_string()
-    title_base = "Plot of " + function_name
-    title = title_base
-    filename = "function_plot.png"
-    xlabel = "x"
-    ylabel = "f(x)"
-    root_data_label = "root approximations"
-    y_func_label = function_name
+            #if NOT < 0 do what is in the notes here
+
+            if i >0:
+                #calc rel_err here and compare to err_stop
+                #if rel_err is less than rel_err stop the loop now
+
+                #if it's not put find a new xns (don't forget to store the old value in'
+                #xns_old
 
 
-    LinePlot111(x,fx,xlabel,ylabel,title,filename)
+
+
+
+
+
+
+
+
+    else:
+        print("Your a and b values do not bracket the root")
+
+
+
+
 
 if __name__ == '__main__':
     main()
---
+++ bisection_in_class.py
@@ -0,0 +1,28 @@
+from math import *
+from psm_plot import *
+
+def f(x):
+    #tmp = 1.2*pow(x,3)+2*pow(x,2)-20*x-10
+    tmp = x - 2.0*exp(-x)
+
+    return tmp
+
+def main():
+    a = 0.0         #define and a and b (bracketing values)
+    b = 1.0
+    dx = .01
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
+
+
+
+
