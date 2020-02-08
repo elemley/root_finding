238f10d7f1bbd8ca11ec6df93d18f8fdeefb24a3
elemley <elemley@gmail.com>
2020-02-08 11:12:42 -0600

    Reorg and cleanup Spring 2020

--- f_string_example.py
+++ f_string_example.py
@@ -1,26 +1,44 @@
 from math import *
-import scipy as sp
+import sympy as sympy
+import numpy as np
 from psm_plot import *
-from random import *
 
 #Note this is specified def f_string():
 def f_string():
-    fn_string = "pow(x,3)-x-exp(x)-1+y"
+    fn_string = "x**3-x-exp(x)-1"
     #fn_string = "-0.3*x**3"
     return fn_string
 
-def f(x,y):
+def f(x):
     string = f_string()
     tmp = eval(string)
     return tmp
 
 def main():
     x = 1.0
-    y = -0.5
-    print(f(x,y))
+    print(f(x))
     print("x=", x)
-    print("y=", y)
-    print("f(x,y)=", f_string(),"=",f(x,y))
+    print(f"f(x)= {f_string()} = {f(x)}")
+
+    xvals = np.linspace(-1,1)
+
+    function_name = f_string()
+    function_syms = sympy.latex(sympy.simplify(function_name))
+    title_base = "Plot of " + "$" + function_syms + "$"
+    title = title_base
+    filename = "fn_string_example.png"
+    xlabel = "x"
+    ylabel = "f(x)"
+    #root_data_label = "root approximations"
+    #y_func_label = function_name
+
+    Function0Plot111(xvals,f,xlabel,ylabel,title,filename)
+
+
+    #FunctionRootPlot111(x, xlabel, f, ylabel, root_data, root_data_label, title, filename)
+
+
+
 
 if __name__ == '__main__':
     main()
--- fixed_point.py
+++ fixed_point.py
@@ -1,18 +1,26 @@
 from math import *
-import scipy as sp
 from psm_plot import *
-from random import *
+from tabulate import tabulate
+import sympy as sympy
+
 
 #Note this is specified def f_string():
-def f_string():
+def g_string():
     #fn_string = "1.2*pow(x,3)+2*pow(x,2)-20*x-10"
     fn_string = "5/(exp(0.5*x)+1.2)"
     return fn_string
 
-def g(x):
-    #tmp = 1.2*pow(x,3)+2*pow(x,2)-20*x-10
-    #tmp = x - 2.0*exp(-x)
+def f_string():
+    fn_string = "x*exp(0.5*x)+1.2*x -5"
+    return fn_string
+
+def f(x):
     string = f_string()
+    tmp = eval(string)
+    return tmp
+
+def g(x):
+    string = g_string()
     tmp = eval(string)
     return tmp
 
@@ -20,23 +28,46 @@ def main():
     xold = 2.0     #initial guess of x
     root_approx = [xold]
 
-    rel_err_stop = 1e-5     #adjust as needed...
-    rel_err = 1.1*rel_err_stop  #set value large enough to start process
+    err_stop = 1e-5     #adjust as needed...
+    rel_err = 1.1*err_stop  #set value large enough to start process
     count = 0
     max_iter = 5
 
-    while rel_err > rel_err_stop and count < max_iter:
+    table = [["i","root approx.","rel. error"]]
+
+    for i in range(0,max_iter):
         count+=1
         tmp = g(xold)
         xnew = tmp
         root_approx.append(xnew)
         if count > 1:
             rel_err = abs((xnew-xold)/xnew)
-        xold=xnew
-        print(count, xold, xnew,rel_err)
-    #print(rel_err)
-    #print(root_approx)
-    #print(xnew)
+            table.append([i + 1, xnew, f"{rel_err:.2e}"])
+            if rel_err <= err_stop:
+                break
+        else:
+            table.append([i+1, xnew, "NA"])
+            # if it's not put find a new xns (don't forget to store the old value in'
+            # xns_old
+        xold = xnew
+
+    print(tabulate(table,tablefmt="fancy_grid", headers="firstrow"))
+
+    x_start = 1
+    x_end = 2.5
+    x = np.linspace(x_start, x_end, 100)
+
+    function_name = f_string()
+    function_syms = sympy.latex(sympy.simplify(function_name))
+    title_base = "Plot of " + "$" + function_syms + "$"
+    title = title_base
+    filename = "fixed_point.png"
+    xlabel = "x"
+    ylabel = "f(x)"
+    root_data_label = "root approximations"
+    y_func_label = function_name
+
+    FunctionRootPlot111(x, xlabel, f, ylabel, root_approx, root_data_label, title, filename)
 
 
 if __name__ == '__main__':
